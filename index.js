d04212d0c24e0a586fa879d300f781951005b750
techsavvyanuj <anujmishra9893@gmail.com>
2025-08-31 15:27:02 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 4000;
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['https://times-frontend.vercel.app', 'https://timesnowindiaadmin-main.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
